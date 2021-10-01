e10e1887ae41c50ea2486690454b33cd423384cd
Marotti13 <austin.marotti13@gmail.com>
2021-10-01 09:30:26 -0500

    started making components into pages - moved S and T to pages section

--- myApp/src/App.tsx
+++ myApp/src/App.tsx
@@ -13,6 +13,7 @@ import { ellipse, square, triangle, flame} from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
+import ScoreAndTicker from './pages/ScoreAndTicker';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -41,8 +42,8 @@ const App: React.FC = () => (
           <Route exact path="/tab1">
             <Tab1 />
           </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
+          <Route exact path="/tab2"> {/**this can be whatever i want, doesnt have to be tab 2. Just letting future me know  */}
+            <ScoreAndTicker />
           </Route>
           <Route path="/tab3">
             <Tab3 />
